1d2e82ba951efa91e47675a7f1cf830c8d6ebeac
AlexanderShmygl <aleksshmygol6@list.ru>
2025-11-20 17:57:16 +0500

    add models

---
+++ models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from uuid import uuid4, UUID
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
+    description: Mapped[str] = mapped_column(nullable=True)
---
+++ settings.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
+from pydantic_settings import BaseSettings
+
+def get_engine(connect_url):
+    engine = create_async_engine(
+        connect_url,
+        echo=True  # Логирование SQL-запросов
+    )
+    return engine
+
+class DBSettings(BaseSettings):
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+    @property
+    def get_sync_connect_str(self) -> str:
+        """Синхронное подключение"""
+        return f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+    @property
+    def get_async_connect_str(self) -> str:
+        """Асинхронное подключение"""
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+#//// другой файл
+settings = DBSettings(
+    host=os.getenv('POSTGRES_HOST'),
+    port=os.getenv('POSTGRES_PORT'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
+    database=os.getenv('POSTGRES_DB'),
+)
+
+engine = get_engine(settings.get_async_connect_str)
+async_session_factory = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
